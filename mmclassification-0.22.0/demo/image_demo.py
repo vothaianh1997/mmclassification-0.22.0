8899bdb84523a2d17a2131ae6a704a8a44fbaf0f
vothaianh1997 <30552121+vothaianh1997@users.noreply.github.com>
2022-04-26 21:32:00 +0700

    Initial commit

---
+++ mmclassification-0.22.0/demo/image_demo.py
@@ -0,0 +1,28 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from argparse import ArgumentParser
+
+from mmcls.apis import inference_model, init_model, show_result_pyplot
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('img', help='Image file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    args = parser.parse_args()
+
+    # build the model from a config file and a checkpoint file
+    model = init_model(args.config, args.checkpoint, device=args.device)
+    for i, stage in enumerate(model.backbone.stages):
+        print(i, len(stage))
+        print(i, stage[0])
+    # test a single image
+    result = inference_model(model, args.img)
+    # show the results
+    show_result_pyplot(model, args.img, result)
+
+
+if __name__ == '__main__':
+    main()
